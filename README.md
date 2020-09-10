d9396c7e2a3efc883db99dde9343ce1e9a1faf5b
igondiu <ion.gondiu@ibm.com>
2020-09-10 20:33:23 +0200

    Readme + unit test

--- README.md
+++ README.md
@@ -3,3 +3,40 @@ Data scraper is a module made to scrap data from an input csv file and insert it
 The project is made to be run on a Docker container.
 
 
+### To run the project :   
+* 1 : Open to the folder `data_scraper` in a terminal window
+
+* 2 : Copy and past this shell command in the terminal window :
+`docker-compose -f devops/docker_compose_data_scraper.yml up --build`  
+If everything went good you should see something similar to this in your terminal window : 
+![alt text](img/TERMINAL_RESULT.png?raw=true "Terminal result")
+
+* 3 : It's time to open the Swagger ! (API's documentation)   
+Copy and paste this link into your web browser : `http://0.0.0.0:5000/doc`
+The swagger must open in your web browser : 
+![alt text](img/SWAGGER.png?raw=true "Terminal result")
+
+
+### Test the module :
+
+* 1 : Use the first endpoint to post a file to the module, it will take the data from the input file and insert into the MySQL database  
+The file is located in the folder `data_scraper/tests/test_data/data_for_tests.csv`  
+
+
+* 2 : Use the second endpoint to check if the data was correctly inserted into MySQL database  
+4 Arguments are required to be able to get data : 
+MCC
+Net  
+Area  
+Cell
+
+To be sure to retrieve at least one row from the database use this :  
+`MCC = 270`  
+`Net = 99`  
+`Area = 12`  
+`Cell = 22222`  
+
+
+### Logs
+Logs are available in the folder `data_scraper/logs`  
+If something doesn't work as expected you can consult the logs
--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -1,7 +1,17 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath("./tests")))
+
 import requests
 import pytest
+from flask import Flask
+from werkzeug.datastructures import FileStorage
+from config import config
 from src.data_manager import DataManager
+from src.main import main
 import config.config as cfg
+
+app = Flask(__name__)
 
 
 @pytest.fixture
@@ -12,7 +22,9 @@ def data_manager():
 def test_post_headers_body_json(data_manager):
     get_url = 'http://0.0.0.0:5000/test_upciti/api/v1/get_cell_towers/'
     post_url = "http://0.0.0.0:5000/test_upciti/api/v1/populate_db"
-
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(root_dir, "../test_data/data_for_tests.csv")
+    print(file_path)
     # Additional headers.
     headers = {'Content-Type': 'application/json'}
 
@@ -22,10 +34,19 @@ def test_post_headers_body_json(data_manager):
     area_arg = 12
     cell_arg = 22222
 
+    # run main.py
+    main(config, app)
+
+    # POST Request :
+
+    with open(file_path, 'rb') as fp:
+        file = FileStorage(fp)
+        resp = requests.get(post_url, file=file, headers=headers)
+        assert resp.status_code == 201
+        print(resp)
+
+    # GET Request :
     get_url = get_url + "'{0}'&'{1}'&'{2}'&'{3}'".format(mcc_arg, net_arg, area_arg, cell_arg)
 
     resp = requests.get(get_url, headers=headers)
-
     assert resp.status_code == 200
-    resp_body = resp.json()
-    assert resp_body['url'] == get_url
